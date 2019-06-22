01d02bf549dba4659e002a4b495d6887a0ee5089
mjdall <morgandally4@gmail.com>
2019-06-22 23:02:57 +1200

    Make font scale with screensize

--- src/components/Style/GlobalStyle.js
+++ src/components/Style/GlobalStyle.js
@@ -5,11 +5,7 @@ export default createGlobalStyle`
     /* @import url('https://fonts.googleapis.com/css?family=Roboto'); */
     font-family: Arial, Helvetica, sans-serif;
     margin: 0;
-  }
-
-  @media (min-width: 750) {
-    body {
-      font-size: 10px;
-    }
-  }
+    font-size: calc(12px + (18px - 12px) * ((100vw - 300px) / (1500px - 300px)));
+    /* line-height: calc(1.3em + (1.5 - 1.2) * ((100vw - 300px)/(1600 - 300))); */
+}
 `
